7e5ab302b20d11fac3e44f0d2fb28bb50130dce7
Vladimír Bíro <vladimir.biro@gmail.com>
2017-03-26 12:36:59 +0200

    Update

---
+++ example/test.php
@@ -0,0 +1,39 @@
+<?php
+namespace VladimirBiro\TestAplication;
+
+use VladimirBiro\Picture\PictureManager;
+
+require __DIR__ . '/bootstrap.php';
+
+
+$pm = new PictureManager();
+
+/** Default parameters - BasePressenter */
+$pm->setPictureRoot(__DIR__ . '/savepic');
+$pm->setType([
+    array('thumb', 150, 150),
+    array('medium', 300, 300),
+    array('big', 600, 600)
+]);
+
+if (isset($_POST))
+{
+    /** Input */
+    $file = $_FILES['file'];
+    $dir = '1';
+    $name = 'Product name';
+
+    echo $pm->savePicture($file, $name, $dir);
+}
+
+
+// Delete picture
+//$pm->deletePicture('1', 'product-name-bsx-20170326122327-87q684nh.jpg');
+// OR Delete all pictures in dir
+// $pm->deletePicture('1');
+?>
+
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit">
+</form>
--- src/PictureManager.php
+++ src/PictureManager.php
@@ -1,9 +1,130 @@
 <?php
 namespace VladimirBiro\Picture;
 
+use Nette\Utils\Strings;
+use Nette\Utils\Image;
+use Nette\Utils\Random;
+use Nette\Utils\Json;
+use Nette\Utils\FileSystem;
+use Nette\Http\FileUpload;
+
 
 class PictureManager
 {
+    private $pictureRoot;
+
+    private $typeArray = array(
+        array('thumb', 140, 140),
+        array('small', 300, 300),
+        array('normal', 600, 600),
+        array('big', 1200, 1000)
+    );
+
+
+    /**
+     * SavePicture
+     */
+    public function savePicture($file, $name, $dir)
+    {
+        $file = new FileUpload($file);
+
+
+
+
+
+        if ($file->isImage() and $file->isOk())
+        {
+            $end = strtolower(mb_substr($file->getSanitizedName(), strrpos($file->getSanitizedName(), ".")));
+
+            $newFileName = $this->namePicture($name);
+
+            $newFileName .= $end;
+
+            $file->move($this->pictureRoot . '/' . $dir . '/' . $newFileName);
+
+            foreach($this->typeArray as $ta)
+            {
+                $thumb = Image::fromFile($this->pictureRoot . '/' . $dir . '/' . $newFileName);
+
+                if($thumb->getWidth() > $thumb->getHeight()) {
+                    $thumb->resize($ta[1], NULL);
+                }
+                else {
+                    $thumb->resize(NULL, $ta[2]);
+                }
+
+                $thumb->sharpen();
+
+                FileSystem::createDir($this->pictureRoot . '/' . $dir . '/' . $ta[0]);
+
+                $thumb->save($this->pictureRoot . '/' . $dir . '/' . $ta[0] . '/'. $newFileName);
+            }
+
+            return Json::encode([
+                'dir' => $dir,
+                'name' => $newFileName
+            ]);
+        }
+
+        return false;
+    }
+
+
+
+    /**
+     * Delete picture
+     */
+    public function deletePicture($dir, $name = null)
+    {
+        if ($name === null)
+        {
+            FileSystem::delete($this->pictureRoot . '/' . $dir);
+        }
+        else
+        {
+            foreach($this->typeArray as $ta)
+            {
+                FileSystem::delete($this->pictureRoot . '/' . $dir . '/' . $ta[0] . '/' . $name);
+            }
+
+            FileSystem::delete($this->pictureRoot . '/' . $dir . '/' . $name);
+        }
+
+    }
+
+
+
+    /**
+     * Vygenerovanie nazvu obrazku
+     */
+    private function namePicture($name)
+    {
+        $fileName = $name . '-' . date('Ymdhis') . '-' . Random::generate(8, 'a-z0-9');
+        $fileName = Strings::webalize($fileName);
+
+        return $fileName;
+    }
+
+
+    /**
+     * Nastavenie sirky
+     */
+    public function setType($type)
+    {
+        $this->typeArray = $type;
+    }
+
+
+    /**
+     * Nastavenie hlavneho adresara s obrazkami
+     */
+    public function setPictureRoot($dir)
+    {
+        $this->pictureRoot = $dir;
+    }
+
+
+
     public function hello()
     {
         return 'Picture Manager Say: Hello world';
